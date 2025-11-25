2685020448a355d05627149495d8e1b456d23a87
Saif Husain <148823252+saifhusain62@users.noreply.github.com>
2025-11-25 19:03:04 +0600

    Add files via upload

--- beecrowed1174.c
+++ beecrowed1174.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    double A[100]; // Declare an array of 100 doubles
+    double A[100];
+    int i;
 
-    // Read 100 floating-point numbers into the array
-    for (int i = 0; i < 100; i++) {
+    for (i = 0; i < 100; i++) {
         scanf("%lf", &A[i]);
     }
 
-    // Iterate through the array and print elements less than or equal to 10.0
-    for (int i = 0; i < 100; i++) {
+    for (i = 0; i < 100; i++) {
         if (A[i] <= 10.0) {
             printf("A[%d] = %.1lf\n", i, A[i]);
         }
